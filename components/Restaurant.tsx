db2d6c623b1c7ab7e9c5b086bc0100ae65ad472d
eliudjgomezg <eliudjgomezg@gmail.com>
2024-09-03 21:31:19 -0400

    Updated: all design

--- components/Restaurant.tsx
+++ components/Restaurant.tsx
@@ -27,37 +27,40 @@ const menuItems = [
 
 const Restaurant = () => {
   return (
-    <div className="text-white bg-[#DA281B]">
+    <div className="text-white bg-white bg-menu">
       <div className="mx-auto max-w-screen-lg">
-        <header className="rounded-b-3xl bg-[#0037A1] py-4 border-b-2 border-white">
-          <h1 className="text-center text-3xl font-bold md:text-4xl lg:text-5xl">Fonda 2024</h1>
+        <header className="rounded-b-3xl bg-blue-transparent py-4 border-b-2 outline-[#DA281B] outline border-white">
+          <h1 className="text-center text-3xl font-bold md:text-4xl lg:text-5xl"> Fonda 2024 </h1>
         </header>
 
         <main className="container mx-auto px-4 py-8">
-          <h4 className="text-center">¡Bienvenid@s a la Fonda 2024 del Colegio Chileno Árabe!</h4>
-          <p className="mt-4 text-center">
-            Te invitamos a visitar nuestro stand <span className="text-[#0037A1] underline font-semibold">{'"PERDONEN NUESTRAS NOTAS"'}</span> y
-            disfrutar de los deliciosos cócteles que hemos preparado especialmente para TI. <br />
-          </p>
-          <h5 className="text-center mt-4 text-[#0037A1] font-bold">¡No te lo pierdas!</h5>
+          <div className="py-6 px-2 rounded-xl bg-white-transparent shadow-md">
+            <h4 className="text-center text-[#0037A1] font-bold">¡Bienvenid@s a la Fonda 2024 del Colegio Chileno Árabe!</h4>
+            <p className="mt-4 text-center text-[#0037A1]">
+              Te invitamos a visitar nuestro stand <br />{' '}
+              <span className="text-[#DA281B] underline font-semibold">{'"PERDONEN NUESTRAS NOTAS"'}</span> <br /> y disfrutar de los deliciosos
+              cócteles que hemos preparado especialmente para TI.
+            </p>
+            <h5 className="text-center mt-4 text-[#0037A1] font-bold">¡No te lo pierdas!</h5>
+          </div>
 
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 mt-8">
             {menuItems.map((item, index) => (
-              <div key={index} className="overflow-hidden border-2 border-[#0037A1] rounded-3xl bg-white shadow-lg">
+              <div key={index} className="overflow-hidden border bg-white-transparent rounded-3xl border-[#0037A1] shadow-lg">
                 <Image src={item.image} alt={item.name} width={400} height={200} className="h-48 w-full object-cover" />
                 <div className="p-4">
-                  <h2 className="mb-2 text-xl text-black font-semibold text-center text-[#0037A1]">{item.name}</h2>
-                  <p className="text-sm text-black">{item.description}</p>
+                  <h2 className="mb-2 text-xl font-semibold text-center underline text-[#0037A1]">{item.name}</h2>
+                  <p className=" text-[#0037A1]">{item.description}</p>
                 </div>
               </div>
             ))}
           </div>
         </main>
 
-        <footer>
-          <a href="https://devix-academy-front.vercel.app" target="_blank">
-            <Image className="md:hidden block" src={bannerMobile} alt="banner" />
-            <Image className="md:block hidden" src={bannerDesktop} alt="banner" />
+        <footer className="mt-8">
+          <a className="block" href="https://devix-academy-front.vercel.app" target="_blank">
+            <Image className="md:hidden block w-full" src={bannerMobile} alt="banner" />
+            <Image className="md:block hidden w-full" src={bannerDesktop} alt="banner" />
           </a>
         </footer>
       </div>
